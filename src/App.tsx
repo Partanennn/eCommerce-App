a80abf3edd86e24b6ca1d5043144b6926b6e5ae2
Aleksi Partanen <aleksi.partanenn17@gmail.com>
2024-04-02 22:31:48 +0300

    Tech: Added react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/Home/HomePage';
 
-const App = () => {
-  return <h1>Web shop Application</h1>;
-};
+const App = () => (
+  <BrowserRouter>
+    {/* NAV */}
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/test" element={<div>Test</div>} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/pages/Home/HomePage.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Web Shop Application</h1>
+    </div>
+  );
+};
+
+export default HomePage;
